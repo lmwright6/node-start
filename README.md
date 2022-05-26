a482d5cd565a7d837dc069a004641f204ed7bf54
lmwright6 <laurenwpersonal@gmail.com>
2022-05-26 18:51:42 -0500

    starter

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# linx-alison-etl
-linx to alison etl
+# to instantiate application
+run 'npm i'
+
+# to start application
+run 'node app.js'
+
+# Helpful notes
+for iterating through data / actions:
+.map((i) => {}) is asynchronous, all indexes will run the function nearly simultaneously
+
+a traditional for loop is not asynchronous
+for (let i =0; i < length; i++) This will wait to resolve the function of prior index before moving to the next
+
+Async / await / promises / resolutions are an important and weird part of node. 
+If you find yourself thinking that you should have data when you don't have it yet, its probably an async issue
--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const api = require("./data-access/linx");
-const {LegislativeDayMover} = require("./movers/legislativeDayMover");
-const {SessionMover} = require("./movers/sessionMover");
+require("dotenv").config();
+const puppeteer = require('puppeteer');
 
-
-
-
-async function moveAllData(){
-    const key = await api.getKey();
-    const movers = []
-    // movers.push(new LegislativeDayMover("HOUSE"));
-    // movers.push(new LegislativeDayMover("SENATE"));
-    movers.push(new SessionMover());
-
-   movers.map((obj) => {
-    moveLegislativeDayData(key, obj);
-   })
+async function doSomething() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.screenshot({ path: 'google.png' });
 
 }
-async function moveLegislativeDayData(key, obj) {
-    await obj.getSourceData(key);
-    obj.cleanMySQLData();
-    console.log("CLEAN DATA", obj.cleanData)
-    await obj.addDestinationData();
-}
+  
 
-
-moveAllData()
+doSomething();
