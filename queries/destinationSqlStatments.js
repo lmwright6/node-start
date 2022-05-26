cc818f60891c734655eda433b7a1e31fb4fac4d3
lmwright6 <laurenwpersonal@gmail.com>
2022-05-26 18:54:45 -0500

    remove unused files'

--- queries/destinationSqlStatments.js
+++
@@ -1,9 +0,0 @@
-const houseLegislativeDaySql = "INSERT INTO `alison-etl`.`LINX-LegislativeDayRef-test` (SessionOid, LegislativeDay, SessionType, CalendarDate, HouseConveneTime, HouseAdjournTime) VALUES ? ON DUPLICATE KEY UPDATE HouseConveneTime=VALUES(HouseConveneTime), HouseAdjournTime=VALUES(HouseAdjournTime);";
-const senateLegislativeDaySql = "INSERT INTO `alison-etl`.`LINX-LegislativeDayRef-test` (SessionOid, LegislativeDay, SessionType, CalendarDate, SenateConveneTime, SenateAdjournTime) VALUES ? ON DUPLICATE KEY UPDATE SenateConveneTime=VALUES(SenateConveneTime), SenateAdjournTime=VALUES(SenateAdjournTime);";
-const sessionSql = "INSERT INTO `alison-etl`.`LINXSession-test` (LinxId, LegislativeDays, Name, StartDate, EndDate, ActiveSession, TermName) VALUES ?;";
-
-module.exports = {
-    sessionSql:sessionSql,
-    houseLegislativeDaySql: houseLegislativeDaySql,
-    senateLegislativeDaySql: senateLegislativeDaySql
-}
